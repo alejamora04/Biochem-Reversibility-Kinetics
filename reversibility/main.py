dd84a3b15d55d462a5817d7fd104509a74949030
Alejandro Morales <103542335+alejamora04@users.noreply.github.com>
2023-04-24 21:07:30 -0500

    Add files via upload
    Initial commit

---
+++ reversibility/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import csv 
+import plate_map
+import load_plate
+
+# Load Dataframes from input files and combine into one data frame
+
+# Confirm Successful loading of plate_map.py
+print("plate_map.py has finished executing.")
+
+# Confirm Successful loading of load_data.py
+print("load_data.py has finished executing.")
