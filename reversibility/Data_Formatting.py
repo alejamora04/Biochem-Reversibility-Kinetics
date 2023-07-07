d721edab18b404ce7fec5e352eced18e23091b77
Alejandro Morales <alejandro.morales04@gmail.com>
2023-07-06 19:23:55 -0500

    Cleaned up repo and added functionality to plot.py

--- reversibility/Data_Formatting.py
+++ reversibility/Data_Formatting.py
@@ -3,7 +3,6 @@ import os
 import platform
 import pandas as pd
 import numpy as np
-import csv 
 
 # File IO Iterate through specified directory to load specified files.
 def get_file_paths(path, file_extension):
@@ -47,64 +46,70 @@ def csv_to_pandas(dataframe, start_position=0):
             consolidated_list.append(j)
     return consolidated_list
 
-# INPUT: Read layout of 384 well plate format and sample locations into pandas df.
-cwd = os.getcwd()
-# Load 384 plate and sample layout from csv
-plate_path = "/input/plate-maps/"
-plate_well_layout = os.path.join(cwd + plate_path)
-# Load Sample readout data from csv file
-sample_data_path = "input/plate-data/"
-sample_plate_inpath = os.path.join(cwd + sample_data_path)
-file_ext = ".csv"
+def main():
+    # INPUT: Read layout of 384 well plate format and sample locations into pandas df.
+    cwd = os.getcwd()
+    # Load 384 plate and sample layout from csv
+    plate_path = "/input/plate-maps/"
+    plate_well_layout = os.path.join(cwd + plate_path)
+    # Load Sample readout data from csv file
+    sample_data_path = "input/plate-data/"
+    sample_plate_inpath = os.path.join(cwd + sample_data_path)
+    file_ext = ".csv"
 
-# CSVs outlining sample well location and plate layout for each plate.
-parsed_paths = get_file_paths(plate_well_layout, file_ext)
-file_names = get_file_names(parsed_paths)
-# Read Layout of 384 well plate and store it into pd Column
-plate_well_layout = parsed_paths[0]
-sample_layout = parsed_paths[1]
-# Plate Data Readouts
-sample_paths = get_file_paths(sample_plate_inpath, file_ext)
-sample_file_names = get_file_names(sample_paths)
+    # CSVs outlining sample well location and plate layout for each plate.
+    parsed_paths = get_file_paths(plate_well_layout, file_ext)
+    file_names = get_file_names(parsed_paths)
+    # Read Layout of 384 well plate and store it into pd Column
+    plate_well_layout = parsed_paths[0]
+    sample_layout = parsed_paths[1]
+    # Plate Data Readouts
+    sample_paths = get_file_paths(sample_plate_inpath, file_ext)
+    sample_file_names = get_file_names(sample_paths)
 
-# [Sample Data Readouts]
-# Delete Extraneuos rows.
-plate_df = pd.read_csv(sample_paths[0], header = None)
-plate_df.drop(index=plate_df.index[0:2], axis = 0, inplace=True)
-plate_df.drop(index=plate_df.index[384], axis = 0, inplace=True)
+    # [Sample Data Readouts]
+    # Delete Extraneuos rows.
+    plate_df = pd.read_csv(sample_paths[0], header = None)
+    plate_df.drop(index=plate_df.index[0:2], axis = 0, inplace=True)
+    plate_df.drop(index=plate_df.index[384], axis = 0, inplace=True)
 
-# Create a time label for the first row of the dataframe.
-list_label = []
-for i in range(0, 61):
-    list_label.append("Abs_min: " + i)
+    # Create a time label for the first row of the dataframe.
+    list_label = []
+    for i in range(0, 61):
+        list_label.append("Abs_min: " + i)
 
-x = "Well"
-list_label.insert(0, x)
+    x = "Well"
+    list_label.insert(0, x)
 
-# Assign list to the column labels
-plate_df.columns=(list_label)
-plate_df.index=np.arange(384)
+    # Assign list to the column labels
+    plate_df.columns=(list_label)
+    plate_df.index=np.arange(384)
 
-# [Well and Sample position manipulations]
-# Read sample position from 384-well plate into Pandas Df 
-sample_location_df = pd.read_csv(sample_layout, header=None)
-consolidated_sample_list = csv_to_pandas(sample_location_df, start_position=1)
+    # [Well and Sample position manipulations]
+    # Read sample position from 384-well plate into Pandas Df 
+    sample_location_df = pd.read_csv(sample_layout, header=None)
+    consolidated_sample_list = csv_to_pandas(sample_location_df, start_position=1)
 
-well_layout_df = pd.read_csv(plate_well_layout, header=None)
-consolidated_wells = csv_to_pandas(well_layout_df)
+    well_layout_df = pd.read_csv(plate_well_layout, header=None)
+    consolidated_wells = csv_to_pandas(well_layout_df)
 
-# Combine Multiple df into single indexed dataframe
-plate_map_df = pd.DataFrame({"Compound": consolidated_sample_list, "Wells": consolidated_wells})
-merged_dataframe = pd.merge(plate_map_df, plate_df, on="Well")
-merged_dataframe["Compound"].replace(" ", np.nan, inplace=True)
-merged_dataframe.dropna(subset=["Compound"], inplace=True)
+    # Combine Multiple df into single indexed dataframe
+    plate_map_df = pd.DataFrame({"Compound": consolidated_sample_list, "Wells": consolidated_wells})
+    merged_dataframe = pd.merge(plate_map_df, plate_df, on="Well")
+    merged_dataframe["Compound"].replace(" ", np.nan, inplace=True)
+    merged_dataframe.dropna(subset=["Compound"], inplace=True)
 
-# Assign name to dataframe to add pivot table indexing as an option
-merged_dataframe.name = sample_file_names[0] 
+    # Assign name to dataframe to add pivot table indexing as an option
+    merged_dataframe.name = sample_file_names[0] 
 
-# OUTPUT: Write Output to CSV file 
-out_path = "output/csv/{sample_file_names[0]}_formatted.csv"
-file_out_path = os.path.join(cwd + out_path)
+    # OUTPUT: Write Output to CSV file 
+    out_path = "output/csv/{sample_file_names[0]}_formatted.csv"
+    file_out_path = os.path.join(cwd + out_path)
 
-plate_map_df.to_csv(out_path)
-print("[+] Format_Data.py has mapped the sample locations to the relevant wells and formatted the read data.")
+    plate_map_df.to_csv(out_path)
+    print("[+] Format_Data.py has mapped the sample locations to the relevant wells and formatted the read data.")
+
+    return
+
+if __name__=='__main__':
+    main()
--- reversibility/plot.py
+++ reversibility/plot.py
@@ -1,58 +1,93 @@
 # Take all of the sample data and plot it using matplotlib
 import os
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import statistics
+import matplotlib.pyplot as plt
+import Data_Formatting as format
 
-# Import combined dataframes into memory to plot
-cwd = os.getcwd
-plot_path = "reversibility _assay/ouput/csv/combine-out/combined-output.csv"
-plate_path = os.join(cwd, plot_path)
-sample_plate_df = pd.read_csv(plate_path, index_col[0])
-print(sample_plate_df.head)
+# TODO: Perform curve fitting to classify readouts as reversible(FAST, SLOW, IRREVERSIBLE)
 
-"""
-Create outline for a simple scatter plot from one sample of data in matplotlib
-Access Data from Pandas Dataframe
+# Take the average Abs data readout for the control samples. Combine data from multiple samples.
+def avg_pd_rows(pd_dataframe):
+    avg_readout = []
+    for i in range(60):
+        data = pd_dataframe["Abs_Min: "+ str(i)].values.flatten().tolist()
+        Avg_Calc = statistics.mean(data)
+        avg_readout.append(Avg_Calc)
+    return avg_readout
 
-X-axis [Generate List of time points from 0-60 minutes]
-Y-axis [Generate list of absorbance values at each time point for a sample]
-"""
-# X-axis
-x_axis = np.arange(0, 60)
+def main():
+    # File IO
+    # [INPUT] Load formatted and cleaned csv to plot
+    cwd = os.getcwd
+    plot_path = "/ouput/csv/"
+    plate_path = os.path.join(cwd + plot_path)
+    file_ext = ".csv"
 
-# Convert Pandas df Rows into list of values
+    # Parse file paths and names refer to Data_Formatting.py for function details
+    plot_data_paths = format.get_file_paths(plate_path, file_ext)
+    plot_names = format.get_file_names(plot_data_paths)
+    formatted_plate = plot_data_paths[0]
+    plate_name = plot_names[0]
 
-# DMSO Control Data
-DMSO = sample_plate_df.iloc[2, 1:61].values.flatten().tolist()
-# No Enzyme control data 
-No_Compound = sample_plate_df.iloc[9, 1:61].values.flatten().tolist()
-no_compound = sample_plate_df.index[9]
+    sample_plate_df = pd.read_csv(formatted_plate, index_col=[1])
 
-# Format compound
-Current_sample = sample_plate_df.ilox[22, 1:61].values.flatten().tolist()
-current_sample = sample_plate_df.index[22]
+    """
 
-# Plot generated data as line plot.
-plt.plot(x, y)
-plt.plot(x, y, 'b-', label=compound)
-plt.plot(x, No_Compound, 'r-', label=no_compound)
-plt.plot(x, Current_sample, 'yo-', label=current_sample)
+    [Scatter Plot Outline]
 
-# As connected scatter plot
-plt.plot(x, DMSO, 'bo-', label="DMSO")
-plt.plot(x, No_Compound, 'ro-', label=no_compound)
-plt.plot(x, Current_sample, 'yo-', label=current_sample)
+    X-axis: Generate List of time points from 0-60 minutes
+    Y-axis: Read plate absorbance data at each time point for a sample
+    DMSO [+] Ctrl: Average DMSO value readouts for plot data.
+    No Enzyme [-] Ctrl: Format no enzyme control.
 
-# Format plot, labels, legends and axis
-plt.xlabel('Time [Minutes]')
-plt.ylabel('Recorded Absorbance')
-plt.title('sample_reversibilitty readout')
-plt.legend(loc='upper right')
-plt.show
 
-# File output
-plot_out_path = "reversibility _assay/ouput/csv/combine-out/combined-output.csv"
-plot_path = os.join(cwd, plot_out_path)
-plt.save_fig(f"plot_path".png)
-print("[+] Plot.py has finished plotting figures. Plots located at:", plot_path)
+    """
+    x_axis = np.arange(0, 60)
+
+    # Convert Pandas df Rows into list of values
+
+    # [DMSO] Extract DMSO readouts into smaller df
+    DMSO_df = sample_plate_df.loc["DMSO"]
+    DMSO_Avg = avg_pd_rows(DMSO_df)
+
+    # [No Enzyme] control data 
+    No_Compound_df = sample_plate_df.loc["no enzyme"]
+    No_Enzyme_Avg = avg_pd_rows(No_Compound_df)
+
+    # Drop [+ & -] Controls to extract sample information
+    Compound_df = sample_plate_df.drop["DMSO", "no enzyme"]
+    
+    # [OUTPUT] Path for generated plots
+    out_path = "/output/plots/"
+    plot_path = os.path.join(cwd + out_path)
+
+    i = 0
+    for i in range(len(Compound_df)):
+        current_sample_ro = Compound_df.iloc[i, 2:62].values.flatten().tolist()
+        current_sample_label = Compound_df.index[i]
+        sample_well = Compound_df.iloc[i, 0]
+
+        # Plot generated data
+        plt.clf()
+        plt.plot(x_axis, DMSO_Avg, 'bo-', label="DMSO n=15")
+        plt.plot(x_axis, No_Enzyme_Avg, 'ro-', label="no_compound n=13")
+        plt.plot(x_axis, current_sample_ro, 'yo-', label= current_sample_label)
+
+        # Format plot, labels, legends and axis
+        plt.xlabel('Time [Minutes]')
+        plt.ylabel('Recorded Absorbance')
+        plt.title(plate_name)
+        plt.legend(loc='upper right')
+        #plt.show()
+
+        # [Checkpoint] Check generated plots
+        plt.savefig(f"{plot_path}Well({sample_well})_Sample_{current_sample_label}.png")
+        i = i + 1
+    
+    print("[+] Plot.py has finished plotting figures. Plots located at:", plot_path)
+    return
+
+if __name__ == '__main__':
+    main()
